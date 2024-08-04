cc82fb5b1a6f2bc6fef10329e0b4fe30739d3e2b
Neil-Lima <110132286+Neil-Lima@users.noreply.github.com>
2024-08-04 18:10:19 -0300

    d

--- backend/src/routes/usuario.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const usuarioController = require('../controllers/usuario.controller');
-
-const router = express.Router();
-
-router.post('/', usuarioController.criarUsuario);
-router.get('/', usuarioController.getUsuarios);
-
-module.exports = router;
